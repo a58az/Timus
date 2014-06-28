1c39ca7b562d2fc9c6675216b41c367c519ad155
a58az <a58az@ya.ru>
2014-06-28 17:20:19 +0300

    Template

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt", stdin);
+#endif
+
+    return 0;
+}
