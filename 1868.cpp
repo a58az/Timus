ea1236861e96ffaab2dee0c9e9cfd401bc4e0dcc
a58az <a58az@ya.ru>
2014-06-28 14:31:32 +0300

    Changed 1868. Prediction Contest
    Add stl

--- 1868.cpp
+++ 1868.cpp
@@ -1,47 +1,63 @@
-#include <iostream>
-#include <iomanip>
 #include <cstdio>
-#include <string>
-#include <map>
-#include <unordered_map>
-#include <vector>
 #include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
 #include <queue>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    #ifndef ONLINE_JUDGE
+int main()
+{
+#ifndef ONLINE_JUDGE
     freopen("input.txt", "rt", stdin);
-    #endif
+#endif
 
     int i, k, n, m, med;
-    string name, medal;
-    unordered_map<string, int> univers;
-    m = 3; n = 4;
-    for (k = 1; k <= m; k++)
+    string medal, name;
+    map<string, int> univers;
+    map<string, int>::iterator it;
+    m = 3; n = 12;
+    for (i = 0; i < n; ++i)
+    {
+        cin >> name;
+        // univers[name] = (i / 4) + 1;
+        univers.insert(pair<string, int>(name, i/4+1));
+    }
+
+    cin >> m;
+    int counter, maxCounter, wA;
+    maxCounter = wA = 0;
+    for (k = 0; k < m; ++k)
+    {
+        cin >> n;
+        counter = 0;
         for (i = 0; i < n; ++i)
         {
-            cin >> name;
-            univers[name] = k;
-        }
-
-    cin >> m;   
-    int maxCounter, wA;
-	maxCounter = wA = 0;
-    for (k = 0; k < m; ++k) { 
-        cin >> n;
-        int counter = 0;
-        for (i = 0; i < n; ++i) {
             cin >> name >> medal >> medal;
-            med = medal[0] == 'g' ? 1 : medal[0] == 's' ? 2 : 3;
-            if (univers[name] == med) {
-                counter++;                
+            switch (medal[0]) {
+                case 'g': med = 1; break;
+                case 's': med = 2; break;
+                default: med = 3; break;
+            }
+            it = univers.find(name);
+
+            if (it->second == med)
+            {
+                counter++;
             }
         }
-        if (counter == maxCounter) {
+
+        if (counter == maxCounter)
+        {
             wA += 1;
-        } else if (counter > maxCounter) {
+        }
+        else if (counter > maxCounter)
+        {
             maxCounter = counter;
             wA = 1;
         }
